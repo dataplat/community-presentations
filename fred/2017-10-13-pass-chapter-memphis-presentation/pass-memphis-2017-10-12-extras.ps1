2297125fcdaee6691fa9018e1e7c9cb8b4c1ee1a
FriedrichWeinmann <friedrich.weinmann@powershell-stuttgart.de>
2017-10-13 03:08:56 +0200

    Uploaded Fred's Presentation on scripting

---
+++ fred/2017-10-13-pass-chapter-memphis-presentation/pass-memphis-2017-10-12-extras.ps1
@@ -0,0 +1,81 @@
+﻿# failsafe
+break
+
+<#
+The dbatools parameter for instances
+
+Its premise: Accept any object that legally represents a database
+
+Benefits:
+ - User does not have to think in types
+ - Easy to implement for contributors
+ - Easy to update for all commands in dbatools
+#>
+
+# We need to explicitly import for this if it's not already imported
+Import-Module dbatools
+
+# A small demo function that shows what reached the inside of the function
+function Get-DbaTest
+{
+    [CmdletBinding()]
+    Param (
+        [Parameter(ValueFromPipeline = $true)]
+        [DbaInstanceParameter[]]
+        $SqlInstance
+    )
+
+    process
+    {
+        foreach ($instance in $SqlInstance)
+        {
+            $instance
+        }
+    }
+}
+
+# Let's try just a computername
+Get-DbaTest -SqlInstance sql2014
+
+# Maybe add an instance name
+Get-DbaTest -SqlInstance sql2014\foo
+
+# This is not doing any external lookups btw
+Get-DbaTest -SqlInstance foo\bar
+
+# Port numbers are ok as well
+Get-DbaTest -SqlInstance "foo,1234"
+
+# Or if you come from the networking side of things
+Get-DbaTest -SqlInstance "foo:1234"
+
+# How about some sql customary usages?
+Get-DbaTest -SqlInstance "."
+
+# Or piped?
+"." | Get-DbaTest
+
+# I've heard dbas hate/love connection strings
+Get-DbaTest -SqlInstance "Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;"
+
+# Remember this path?
+Get-DbaTest -SqlInstance "\\.\pipe\sql\query"
+Get-DbaTest -SqlInstance "\\foo\pipe\sql\query"
+Get-DbaTest -SqlInstance '\\foo\pipe\MSSQL$bar\sql\query'
+
+# Dns Resolutions?
+[System.Net.Dns]::Resolve("sql2016")
+[System.Net.Dns]::Resolve("sql2016") | Get-DbaTest
+
+# Active Directory Computer?
+Get-ADComputer sql2016 | Get-DbaTest
+
+<#
+Key takeaway:
+If you think an object - the output of a command,
+or an object of a given type - should be legal input
+to target an instance, it probably is.
+
+It's also easy to add.
+So don't hesitate to ask for the feature!
+#>
---
+++ fred/2017-10-13-pass-chapter-memphis-presentation/pass-memphis-2017-10-12.ps1
@@ -0,0 +1,187 @@
+﻿# Failsafe
+break
+
+# Step 1: Introductions
+# Step 2: Context
+<#
+Objectives
+
+dbatools vs. SqlServer Module
+- History
+- Positioning / Role
+
+The Community
+- dbatools.io
+- github
+- slack
+
+                                    dbatools Resources
+                       dbatools.io/commands | List of commands
+                        dbatools.io/offline | Installation instructions
+                          dbatools.io/slack | Get in contact with us
+                          dbatools.io/agent | Setting up PowerShell agent tasks (See Step 8)
+github.com/sqlcollaborative/dbatools/issues | Report a bug, bring a feature request
+#>
+
+#---------------------------#
+# Step 3: PowerShell basics #
+
+# a) Google for PowerShell
+Get-Command | Measure-Object
+Get-Command *database*
+# Hint: Use Verbs
+Get-Command Get-*database*
+# Hint2: Use Prefix
+Get-Command Get-Dba*database*
+
+# b) Help!
+Get-Help dir
+Get-Help dir -Examples
+
+
+#-------------------------------#
+# Step 4: dbatools preparations #
+
+<#
+Prerequisites
+- PowerShell v3+
+- When running on SQL Server directly: Elevation ("Run As Administrator" ; some commands require it for running against local server)
+#>
+
+# Requires Internet and not available by default on older PowerShell versions
+Install-Module dbatools
+# See website for install instructions if this is not an option for you
+
+Import-Module dbatools
+
+
+#----------------------------#
+# Step 5: Database Migration #
+Get-Command Copy-Dba*
+Get-Help Copy-DbaDatabase -Examples
+
+# Check source Database
+Get-DbaDatabase -SqlInstance sql2014 | ft
+Get-DbaDatabase -SqlInstance sql2014 -Database DBA
+
+# Check destination for Database
+Get-DbaDatabase -SqlInstance sql2016 -Database DBA
+
+# Do it
+Copy-DbaDatabase -Source sql2014 -Destination sql2016 -Database DBA -BackupRestore -NetworkShare \\sql2014\Migration
+Get-DbaDatabase -SqlInstance sql2016 | ft
+Get-DbaDatabase -SqlInstance sql2016 -Database DBA
+
+# OK, maybe undo the change before Chrissy notices
+Get-DbaDatabase -SqlInstance sql2016 -Database DBA | Remove-DbaDatabase
+
+# Dummy-Code, but demonstrates scaling up
+Import-Csv C:\migration\mapping.csv | ForEach-Object {
+    Copy-DbaDatabase -Source $_.SourceInstance -Destination $_.DestinationInstance -Database $_.Database -BackupRestore -NetworkShare \\sql2014\Migration
+}
+
+
+#------------------------------#
+# Step 6: Maintenance & Health #
+
+# Connect to instance
+$server = Connect-DbaInstance -SqlInstance sql2014
+# Silence is golden, exception is OH NOES!
+$server.Query("dbcc checkdb ([DBA])")
+
+# Who likes waiting?
+Get-DbaWaitStatistic -SqlInstance sql2016 -IncludeIgnorable | ft
+
+# Ever heard of Glenn Berry?
+# This would run them all, but we don't have the time for it
+$tests = Invoke-DbaDiagnosticQuery -SqlInstance sql2016
+$tests | ft
+# Don't want to run the entire set of queries?
+Invoke-DbaDiagnosticQuery -SqlInstance sql2016 -UseSelectionHelper
+# There's also an export available. Will create a CSV per test in folder
+Invoke-DbaDiagnosticQuery -SqlInstance sql2016 -UseSelectionHelper | Export-DbaDiagnosticQuery -Path C:\temp\glenn
+explorer C:\temp\Glenn
+
+# Who is active on my system?!
+Install-DbaWhoIsActive -SqlInstance sql2016 -Database master
+Find-DbaStoredProcedure -SqlInstance sql2016 -IncludeSystemDatabases -Database master -Pattern "Adam Machanic"
+Invoke-DbaWhoisActive -SqlInstance sql2016 -ShowSystemSpids | ft
+get-help Get-DbaRegisteredServer -Examples
+Get-DbaRegisteredServer -SqlInstance sqlserver2014a | Install-DbaWhoIsActive -Database master
+# Adam Machanic, supporting dbas since 2007 and still rocking
+# Visit him on http://whoisactive.com
+
+# What size is this?
+Get-DbaDatabaseSpace -SqlInstance sql2016 -IncludeSystemDBs | Out-GridView
+Get-DbaDiskSpace -ComputerName sql2014 -CheckForSql
+
+
+#--------------------------#
+# Step 7: Backup & Restore #
+
+# When was the last backup?
+Get-DbaBackupHistory -SqlInstance sql2016 -Last
+
+# All in one command
+Get-Help Test-DbaLastBackup -Online
+Test-DbaLastBackup -SqlInstance sql2016 | Out-GridView
+
+# This would theoretically work, but take a bit right now
+Get-DbaRegisteredServer -SqlInstance sqlserver2014a | Test-DbaLastBackup
+
+
+#---------------------------------------#
+# Step 8: PowerShell Jobs in SQL Server #
+
+<#
+Three ways to schedule tasks on a Sql Server
+
+Windows Scheduled Tasks
+- Requires local admin
+- No reporting/discovery via Sql Server
+- All infrastructure must be built by yourself
+
+SQL Agent PowerShell Job Step
+- Fixed Execution Policy
+- Fixed PowerShell version
+- Weird behavior, things will break
+--> DON'T DO IT, JIM! <--
+
+SQL Agent CmdExec Job Step running powershell.exe
+- Respects system settings for PowerShell
+- Always current version
+- Reproducable results from interactive use, provided privileges are correct
+--> Way to go
+
+Related blog post:
+https://dbatools.io/agent/
+#>
+
+
+#----------------------------------------#
+# Step 9: Scripting Gotchas for dbatools #
+
+# a) Exceptions are Opt-In
+# Warning is nice and readable
+Get-DbaSqlService -ComputerName doesntexist
+# Let's try/catch that
+try { Get-DbaSqlService -ComputerName doesntexist }
+catch { "Failed" }
+# Didn't work that well, did it?
+# Give it the Silent Treatment!
+try { Get-DbaSqlService -ComputerName doesntexist -Silent }
+catch { "Failed" }
+
+# Note:
+# Parameter will soon be renamed to '-EnableException' to be more intuitive to use
+# '-Silent' will keep working
+
+# b) Tab Expansion and the quest for usability
+Get-DbaDatabase -SqlInstance sql2016 -Database Search_Service_Application_AnalyticsReportingStoreDB_21b749e8b05e470aa0e6bb5360a6bfd5
+
+# Lots of options available
+Get-DbaConfig
+# Looking at something of interest
+Get-DbaConfig -FullName tabexpansion.disable
+# Since that's not necessary, let's shut it down
+Set-DbaConfig tabexpansion.disable $true
