09bc129060dcd3ff5f6dc45072d127fca156bfaf
Chrissy LeMaire <clemaire@gmail.com>
2018-02-18 12:44:38 +0100

    updates

--- rob-sewell-chrissy-lemaire/dbatools-demo.ps1
+++ rob-sewell-chrissy-lemaire/dbatools-demo.ps1
@@ -8,8 +8,10 @@ $allservers = $old, $new
 # Alternatively, use Registered Servers? 
 Get-DbaRegisteredServer -SqlInstance $instance | Out-GridView
 
+
 # Quick overview of commands
 Start-Process https://dbatools.io/commands
+
 
 # You've probably heard about how easy migrations can be with dbatools. Here's an example 
 $startDbaMigrationSplat = @{
@@ -25,8 +27,10 @@ $startDbaMigrationSplat = @{
 
 Start-DbaMigration @startDbaMigrationSplat -Force | Select * | Out-GridView
 
+
 # Use Ola Hallengren's backup script? We can restore an *ENTIRE INSTNACE* with just one line
-Get-ChildItem -Directory \\workstation\backups\sql2012 | Restore-DbaDatabase -SqlInstance $new
+Get-ChildItem -Directory \\workstation\backups\sql2012 | Restore-DbaDatabase -SqlInstance $new -WithReplace
+
 
 # Nowadays, we don't just backup databases. Now, we're backing up logins
 Export-DbaLogin -SqlInstance $instance -Path C:\temp\logins.sql
@@ -34,26 +38,29 @@ Invoke-Item C:\temp\logins.sql
 
 # And Agent Jobs
 Get-DbaAgentJob -SqlInstance $old | Export-DbaScript -Path C:\temp\jobs.sql
+Invoke-Item C:\temp\jobs.sql
+
 
 # What if you just want to script out your restore?
 Get-ChildItem -Directory \\workstation\backups\subset\ | Restore-DbaDatabase -SqlInstance $new -OutputScriptOnly -WithReplace | Out-File -Filepath c:\temp\restore.sql
 Invoke-Item c:\temp\restore.sql
 
 
-# Complaint: Already have a library of Profiler templates
-# Answer: Convert them instantly to Sessions (h/t Jonathan Kehayias)
-Get-DbaTrace -SqlInstance $old | ConvertTo-DbaXESession | Start-DbaXESession
+
+# Reads trace files - default trace by default
+Read-DbaTraceFile -SqlInstance $instance | Out-GridView
+
+# Convert Traces to XE (h/t Jonathan Kehayias)
+Get-DbaTrace -SqlInstance $old | ConvertTo-DbaXESession -Name 'Default Trace' | Start-DbaXESession
 
 # Easily import
-Get-DbaXESessionTemplate | Out-GridView -PassThru | Import-DbaXESessionTemplate -SqlInstance $servers |
+Get-DbaXESessionTemplate | Out-GridView -PassThru | Import-DbaXESessionTemplate -SqlInstance $allservers |
 Start-DbaXESession
 
-# Testing your backups is crazy easy! 
-Start-Process https://dbatools.io/Test-DbaLastBackup
-Test-DbaLastBackup -SqlInstance localhost\sql2017 | Out-GridView
+# XEs Read and watch
+Get-DbaXESession -SqlInstance $new -Session system_health | Read-DbaXEFile
 
-# But what if you want to test your backups on a different server?
-Test-DbaLastBackup -SqlInstance $old -Destination $new | Out-GridView
+
 
 # Know how snapshots used to be a PITA? Now they're super easy
 New-DbaDatabaseSnapshot -SqlInstance $new -Database db1 -Name db1_snapshot
@@ -62,20 +69,27 @@ Get-DbaProcess -SqlInstance $new -Database db1 | Stop-DbaProcess
 Restore-DbaFromDatabaseSnapshot -SqlInstance $new -Database db1 -Snapshot db1_snapshot
 Remove-DbaDatabaseSnapshot -SqlInstance $new -Snapshot db1_snapshot # or -Database db1
 
+
+
 # We evaluated 37,545 SQL Server stored procedures on 9 servers in 8.67 seconds!
 $new | Find-DbaStoredProcedure -Pattern dbatools
 
 # Have an employee who is leaving? Find all of their objects.
 $allservers | Find-DbaUserObject -Pattern loulou | Out-GridView
  
+
+
 # Find detached databases, by example
-Detach-DbaDatabase -SqlInstance $instance -Database AdventureWorks2012
-Find-DbaOrphanedFile -SqlInstance $instance | Out-GridView
+Detach-DbaDatabase -SqlInstance $instance -Database SharePoint_Config
+Find-DbaOrphanedFile -SqlInstance $instance | Out-GridView -PassThru | Select -Expand FileName | Remove-Item
+
 
 # View and change service account
 Get-DbaSqlService -ComputerName workstation | Out-GridView
 Get-DbaSqlService -ComputerName workstation | Select * | Out-GridView
 Get-DbaSqlService -Instance SQL2016 -Type Agent | Update-DbaSqlServiceAccount -Username 'Local system'
+
+
 
 # Check out how complete our sp_configure command is
 Get-DbaSpConfigure -SqlInstance $new | Out-GridView
@@ -84,12 +98,13 @@ Get-DbaSpConfigure -SqlInstance $new -ConfigName XPCmdShellEnabled
 # Easily update configuration values
 Set-DbaSpConfigure -SqlInstance $new -ConfigName XPCmdShellEnabled -Value $true
 
-# XEs Read and watch
-Get-DbaXESession -SqlInstance $new -Session system_health | Read-DbaXEFile
+
 
 # Reset-DbaAdmin
 Reset-DbaAdmin -SqlInstance $instance -Login sqladmin -Verbose
 Get-DbaDatabase -SqlInstance $instance -SqlCredential (Get-Credential sqladmin)
+
+
 
 # sp_whoisactive
 Install-DbaWhoIsActive -SqlInstance $instance -Database master
@@ -102,10 +117,13 @@ Invoke-Item $home
 # Ola, yall
 $instance | Install-DbaMaintenanceSolution -ReplaceExisting -BackupLocation C:\temp -InstallJobs
 
+
+
 # Get db space AND write it to table
 Get-DbaDatabaseFile -SqlInstance $instance | Out-GridView
 Get-DbaDatabaseFile -SqlInstance $instance -IncludeSystemDB | Write-DbaDataTable -SqlInstance $instance -Database tempdb -Table DiskSpaceExample -AutoCreateTable
 Invoke-DbaSqlcmd -ServerInstance $instance -Database tempdb -Query 'SELECT * FROM dbo.DiskSpaceExample' | Out-GridView
+
 
 # History
 Get-Command -Module dbatools *history*
@@ -114,22 +132,23 @@ Get-Command -Module dbatools *history*
 Get-DbaAgentJobHistory -SqlInstance $instance | Out-GridView
 Get-DbaBackupHistory -SqlInstance $new | Out-GridView
 
-# Test/Set SQL max memory
-$allservers | Get-DbaMaxMemory
-$allservers | Test-DbaMaxMemory | Format-Table
-$allservers | Test-DbaMaxMemory | Where-Object { $_.SqlMaxMB -gt $_.TotalMB } | Set-DbaMaxMemory -WhatIf
-Set-DbaMaxMemory -SqlInstance $instance -MaxMb 1023
-
-# Reads trace files - default trace by default
-Read-DbaTraceFile -SqlInstance $instance | Out-GridView
 
 # don't have remoting access? Explore the filesystem. Uses master.sys.xp_dirtree
 Get-DbaFile -SqlInstance $instance
 Get-DbaFile -SqlInstance $instance -Depth 3 -Path 'C:\Program Files\Microsoft SQL Server' | Out-GridView
 New-DbaSqlDirectory -SqlInstance $instance  -Path 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\test'
 
+
 # Out-GridView madness <3
 Get-DbaDatabase -SqlInstance $old | Out-GridView -PassThru | Copy-DbaDatabase -Destination $new -BackupRestore -NetworkShare \\workstation\c$\temp -Force
+
+
+# Test/Set SQL max memory
+$allservers | Get-DbaMaxMemory
+$allservers | Test-DbaMaxMemory | Format-Table
+$allservers | Test-DbaMaxMemory | Where-Object { $_.SqlMaxMB -gt $_.TotalMB } | Set-DbaMaxMemory -WhatIf
+Set-DbaMaxMemory -SqlInstance $instance -MaxMb 1023
+
 
 # We've even got our own config system!
 Get-DbaConfig | Out-GridView
