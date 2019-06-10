694819d3855b35848b25a62d91c51f307c5327c0
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2019-06-10 12:59:19 +0000

    Psconf (#10)
    * add some things
    
    * small changes
    
    * updates
    
    * update
    
    * hayle yea
    
    * add some masking
    
    * update
    
    * pupdates
    
    * cleanup
    
    * update
    
    * pupdates
    
    * change to shipped
    
    * update
    
    * pupdate
    
    * pupdates
    
    * fix json
    
    * pupdate
    
    * updates
    
    * updates
    
    * updates
    
    * adding sharedpath
    
    * update with video

--- chrissy-lemaire/psconf-10-cant-miss.ps1
+++ chrissy-lemaire/psconf-10-cant-miss.ps1
@@ -1,32 +1,62 @@
-﻿# Basics - refresh
+﻿break
+#region Basics
 
-# Add psdbatools, install azure data studio 
-Get-DbaRegisteredServer | Format-Table
+# Get-DbaRegisteredServer, aliased
+Get-DbaRegisteredServer
+Get-DbaRegisteredServer -SqlInstance localhost\sql2016 -IncludeLocal
+Get-DbaRegisteredServer -Group onprem | Get-DbaDatabase | Select SqlInstance, Name | Format-Table -AutoSize
 
-# Connect-DbaInstance
-Get-DbaRegisteredServer -Name psdbatools.database.windows.net | Connect-DbaInstance
 
-# CSV galore! Delete Maximo Park
-Get-ChildItem C:\csv
-Get-ChildItem C:\csv | Import-DbaCsv -SqlInstance localhost\sql2017 -Database tempdb -AutoCreateTable -Encoding  ([System.Text.Encoding]::UTF8)
 
-# Query
-# displays messages (aka print or raiserror) nicely, without interfering with the resultset, and asynchronously
+
+# Connect-DbaInstance, supports everything!
+Get-DbaRegisteredServer -Name azuresqldb | Connect-DbaInstance
+
+
+
+
+# CSV galore!
+Get-ChildItem C:\temp\psconf\csv
+Get-ChildItem C:\temp\psconf\csv | Import-DbaCsv -SqlInstance localhost\sql2017 -Database tempdb -AutoCreateTable -Encoding UTF8
 Invoke-DbaQuery -SqlInstance localhost\sql2017 -Database tempdb -Query "Select top 10 * from [jmfh-year]"
 
+
+
+# Write-DbaDbTableData
+Get-ChildItem -File | Write-DbaDbTableData -SqlInstance localhost\sql2017 -Database tempdb -Table files -AutoCreateTable
+Get-ChildItem -File | Select *
+Invoke-DbaQuery -SqlInstance localhost\sql2017 -Database tempdb -Query "Select * from files"
+
+#endregion
+
+
+
+
 #region Must Haves
+
+# Gotta find it, run this once
 Find-DbaInstance -ComputerName localhost
 
 
-Invoke-DbaDbMasking / Invoke-DbaDbDataGenerator
-Invoke-DbaLogShipping - enables VLDB migs
 
 
+
+# PII Management
+Invoke-DbaDbPiiScan -SqlInstance localhost\sql2017 -Database AdventureWorks2014 | Out-GridView
+
+# Mask that
+New-DbaDbMaskingConfig -SqlInstance localhost\sql2017 -Database AdventureWorks2014 -Table EmployeeDepartmentHistory, Employee -Path C:\temp | Invoke-Item
+Invoke-DbaDbDataMasking -SqlInstance localhost\sql2017 -FilePath 'C:\github\community-presentations\chrissy-lemaire\mask.json' -ExcludeTable EmployeeDepartmentHistory
+
+
+
+
+# Very Large Database Migration
 $params = @{
     Source                          = 'localhost'
     Destination                     = 'localhost\sql2017'
-    Database                        = 'bigoldb'
-    BackupNetworkPath               = '\\localhost\backups'
+    Database                        = 'shipped'
+    SharedPath                      = '\\localhost\backups'
     BackupScheduleFrequencyType     = 'Daily'
     BackupScheduleFrequencyInterval = 1
     CompressBackup                  = $true
@@ -36,55 +66,117 @@ $params = @{
     Force                           = $true
 }
 
-# pass the splat
+
 Invoke-DbaDbLogShipping @params
-Invoke-DbaDbLogShipRecovery -SqlInstance localhost\sql2017 -Database bigoldb
 
-Install-DbaInstance / Update-DbaInstance -> II Video
-#endregion
+# Recover when ready
+Invoke-DbaDbLogShipRecovery -SqlInstance localhost\sql2017 -Database shipped
 
-#region Combo kills
 
-# Start-DbaMigration wraps 30+ commands
-Start-DbaMigration -Source localhost -Destination sql2016 -UseLastBackup -Exclude BackupDevices | Out-GridView
 
-# Wraps
-Export-DbaInstance -SqlInstance localhost\sql2017 -Path C:\temp\dr
-Get-ChildItem -Path C:\temp\dr -Recurse -Filter *database* | Invoke-Item
 
-# Wraps a bunch
-Test-DbaLastBackup -SqlInstance localhost -Destination localhost\sql2016 | Select * | Out-GridView
+# Install-DbaInstance / Update-DbaInstance
+Update-DbaInstance -ComputerName sql2017 -Path \\dc\share\patch -Credential base\ctrlb
+Invoke-Item 'C:\temp\psconf\Patch several SQL Servers at once using Update-DbaInstance by Kirill Kravtsov.mp4'
 
-# All in one, no hassle
-New-DbaAvailabilityGroup
+
 
 #endregion
+
+
+
+
 
 #region fan favorites
 
-# Diagnostic, add connection, preopen
-New-DbaDiagnosticAdsNotebook -TargetVersion 2017 -Path c:\temp\myNotebook.ipynb | Invoke-Item
-Invoke-DbaDiagnosticQuery -SqlInstance localhost\sql2017 | Export-DbaDiagnosticQuery
+# Spaghetti!
+New-DbaDiagnosticAdsNotebook -TargetVersion 2017 -Path C:\temp\myNotebook.ipynb | Invoke-Item
 
-# Dope - https://dbatools.io/timeline/
+
+
+
+# Dope - dbatools.io/timeline
 Get-DbaAgentJobHistory -SqlInstance localhost\sql2017 -StartDate '2016-08-18 00:00' -EndDate '2018-08-19 23:59' -ExcludeJobSteps | ConvertTo-DbaTimeline | Out-File C:\temp\DbaAgentJobHistory.html -Encoding ASCII
 Invoke-Item -Path C:\temp\DbaAgentJobHistory.html
 
 # Prettier
 Start-Process https://dbatools.io/wp-content/uploads/2018/08/Get-DbaAgentJobHistory-html.jpg
 
+
 #endregion
 
-ConvertTo-DbaXESession
-Test-DbaDbCompression
 
-# BONUS: Invoke-DbatoolsRenameHelper
-<#
-    2 for sure, 1 I think it's a feature, most will think it's a limitation.
-    1) it's the only way to issue parametrized statements without incurring in sql-injection problems
-    2) it's a different beast from invoke-sqlcmd, which cannot do 1) BUT can handle the "sqlcmd" scripts, albeit with lots of limits. tl;dr: sqlcmd works with 100%, invoke-sqlcmd works for 80%. Invoke-DbaQuery is NOT invoke-sqlcmd, nor tends to be
-    3) displays messages (aka print or raiserror) nicely, without interfering with the resultset, and asynchronously (think, post 1.0, being able to use ola's maintenance seeing the progress in realtime) (edited)
-    4) database isolation (and that's the feature, at least for me). Being able to invoke a script without parsing it beforehand to inspect if, e.g., will drop msdb , to me is golden.
-    you point it to a user database and you're certain it won't cross boundaries
-    (surely, the only way to be certain would be to use a lowprivileged user, but, still, has a lot better isolation than sqlcmd or invoke-sqlcmd )
-#>
+
+#region Combo kills
+
+
+# Availability Groups
+Invoke-Item 'C:\temp\psconf\click-a-rama.mp4'
+
+# All in one, no hassle - includes credentials!
+$docker1 = Get-DbaRegisteredServer -Name dockersql1
+$docker2 = Get-DbaRegisteredServer -Name dockersql2
+
+# setup a powershell splat (has docker been reset?)
+$params = @{
+    Primary = $docker1
+    Secondary = $docker2
+    Name = "test-ag"
+    Database = "pubs"
+    ClusterType = "None"
+    SeedingMode = "Automatic"
+    FailoverMode = "Manual"
+    Confirm = $false
+ }
+ 
+# execute the command
+ New-DbaAvailabilityGroup @params
+
+
+
+
+# Start-DbaMigration wraps 30+ commands
+Start-DbaMigration -Source localhost -Destination localhost\sql2016 -UseLastBackup -Exclude BackupDevices, SysDbUserObjects -WarningAction SilentlyContinue | Out-GridView
+
+
+
+
+# Wraps like 20
+Export-DbaInstance -SqlInstance localhost\sql2017 -Path C:\temp\dr
+Get-ChildItem -Path C:\temp\dr -Recurse -Filter *database* | Invoke-Item
+
+#endregion
+
+
+
+
+
+#region BONUS
+Get-ChildItem C:\github\community-presentations\*ps1 -Recurse | Invoke-DbatoolsRenameHelper | Out-GridView
+#endregion
+
+
+
+
+
+
+
+
+# if there's time
+
+# Fan favorites
+# Diagnostic
+Invoke-DbaDiagnosticQuery -SqlInstance localhost\sql2017 | Export-DbaDiagnosticQuery -Outvariable exports
+$exports | Select -Skip 3 -First 1 | Invoke-Item
+
+
+# Ola Hallengren supported
+Install-DbaMaintenanceSolution -SqlInstance localhost, localhost\sql2016, localhost\sql2017 -ReplaceExisting -InstallJobs
+
+# ConvertTo-DbaXESession
+Get-DbaTrace -SqlInstance localhost\sql2017 -Id 1 | ConvertTo-DbaXESession -Name 'Default Trace' | Start-DbaXESession
+
+
+ # Wraps a bunch
+Test-DbaLastBackup -SqlInstance localhost -Destination localhost\sql2016 | Select * | Out-GridView
+
