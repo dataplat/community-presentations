21908edc622659e6b5f814892802ec5e9d3c8940
Andy Levy <andy@levyclan.us>
2021-10-23 09:14:46 -0400

    Add errorlog story

--- andy-levy/BackupBasics/06_Backup_Setup.ps1
+++ andy-levy/BackupBasics/06_Backup_Setup.ps1
@@ -8,6 +8,8 @@ Clear-Host;
 
 Get-DbaDefaultPath -SqlInstance FLEXO\Sql17;
 
+Get-DbaDefaultPath -SqlInstance FLEXO\Sql17,FLEXO\Sql19 | Select-Object ComputerName,ErrorLog;
+
 <#
 ## Check & Set Backup Compression
 ### Functions Demonstrated
